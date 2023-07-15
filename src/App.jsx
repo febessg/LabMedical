59cc3300d7bd8f756eee701331280ec3258e2069
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-15 18:26:16 -0300

    Criando service de usuários e LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { LoginPage } from "./pages/Login/Login.page"
 import { HomePage } from "./pages/Home/Home.page"
+import { LocalStorageService } from "./services/User/LocalStorage.service"
+
+if (!LocalStorageService.get('users')) {
+  LocalStorageService.set('users', [
+    {
+        id: 1,
+        email: 'admin@labmedical.com',
+        password: '12345678'
+    },
+    {
+        id: 2,
+        email: 'usuario@labmedical.com',
+        password: '12345678'
+    }
+]
+)
+}
 
 function App() {
   return (
