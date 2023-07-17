65372f411782c81f4579c279a6ca62deaa655a37
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-16 21:06:05 -0300

    Criando contexto de usuário

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import { GlobalStyle } from './global.style.jsx'
 import { AuthProvider } from './contexts/auth/auth.context.jsx'
 import { ToolbarProvider } from './contexts/Toolbar/Toolbar.context.jsx'
+import { UserContext } from './contexts/User/User.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle/>
     <AuthProvider>
-      <ToolbarProvider>
-        <App />
-      </ToolbarProvider>
+      <UserContext>
+        <ToolbarProvider>
+          <App />
+        </ToolbarProvider>
+      </UserContext>
     </AuthProvider>
   </React.StrictMode>,
 )
