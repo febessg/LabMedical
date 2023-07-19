f1ca81aa17890b106cd670e702b2d0551eb42577
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-18 21:28:51 -0300

    Configurando toolbar

--- src/pages/PatientRegister/PatientRegister.page.jsx
+++ src/pages/PatientRegister/PatientRegister.page.jsx
@@ -1,6 +1,27 @@
+import { useContext, useEffect } from "react";
+import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component";
+import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
+import { UserContext } from "../../contexts/User/User.context";
+
 export const PatientRegisterPage = () => {
+    const {setToolbar} = useContext(ToolbarContext);
+    const {user} = useContext(UserContext);
+
+    console.log(user)
+
+    const setHomeToolbar = () => {
+        const newToolbar = {
+            title: 'Cadastrar Paciente',
+            userName: user.name
+        };
+        setToolbar(newToolbar)
+    };
+
+    useEffect(() => setHomeToolbar(), []);
+
     return (
         <>
+            <ToolbarComponent/>
             <p>PatientRegister Page is render</p>
         </>
     )
