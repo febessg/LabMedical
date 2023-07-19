d82242068b04f961a5e97790c9193b3553569fcd
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-19 00:12:56 -0300

    Estilizando formulário de cadastro de paciente

---
+++ src/components/Form/PatientRegister/PatientRegister.style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Form = styled.div`
+    border-radius: 10px;
+    background-color: #FFF;
+    box-shadow: 0px 0px 10px 2px rgba(82, 129, 220, 0.25);
+    width: 100%;
+    box-sizing: border-box;
+`
+
+export const InputGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 0 3rem;
+`
+
+export const Titles = styled.h3`
+    color: #25232F;
+    font-weight: 700;
+    font-size: 1.3rem;
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+`
+export const InputRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+`
