f5b28eceac1cf5a69a04b4e6fccc50d907d0d172
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-20 16:13:55 -0300

    Estilizando página de cadastro de consulta

--- src/pages/Appointments/Appointments.page.jsx
+++ src/pages/Appointments/Appointments.page.jsx
@@ -1,7 +1,43 @@
+import { InputComponent } from "../../components/Form/Input/Input.component";
+import {FaSearch} from 'react-icons/fa';
+import * as Styled from './Appointments.style';
+import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component";
+import { useContext, useEffect } from "react";
+import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
+import { UserContext } from "../../contexts/User/User.context";
+import { UserService } from "../../services/User/User.service";
+
 export const AppointmentsPage = () => {
+    const {setToolbar} = useContext(ToolbarContext);
+    const {user} = useContext(UserContext);
+
+    const setHomeToolbar = () => {
+        const newToolbar = {
+            title: 'Cadastrar Consultas',
+            userName: user.name
+        };
+        setToolbar(newToolbar)
+    };
+
+    useEffect(() => setHomeToolbar(), []);
     return(
         <>
-            <p>Appointment page is render</p>
+        <ToolbarComponent/>
+        <Styled.Appointments>
+            <Styled.Title>Encontre o paciente</Styled.Title>
+            <Styled.SearchPatient>
+                <Styled.InputGroup>
+                    <InputComponent
+                        type='text'
+                        id='pacientSearch'
+                        placeholder='Digite o nome do paciente'
+                    />
+                    <Styled.InputIcon><FaSearch/></Styled.InputIcon>
+                </Styled.InputGroup>
+                <Styled.Button>Buscar</Styled.Button>
+            </Styled.SearchPatient>
+        </Styled.Appointments>
         </>
+        
     )
 }
