3999423827e36ec920e2a57c8ce3addacc3f545c
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-20 20:47:12 -0300

    fix importação duplicada

--- src/contexts/auth/auth.context.jsx
+++ src/contexts/auth/auth.context.jsx
@@ -1,5 +1,4 @@
-import { createContext } from "react";
-import { useState } from "react";
+import { createContext, useState} from "react";
 import PropTypes from 'prop-types';
 
 export const AuthContext = createContext({
