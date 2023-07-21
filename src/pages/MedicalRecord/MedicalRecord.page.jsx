f7b26e98a1e46b93900703057d6532462fc2a3ff
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-21 20:20:07 -0300

    Iniciando estilização pádina de prontuário

--- src/pages/MedicalRecord/MedicalRecord.page.jsx
+++ src/pages/MedicalRecord/MedicalRecord.page.jsx
@@ -1,7 +1,35 @@
+import { useContext, useEffect } from "react";
+import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component"
+import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
+import { UserContext } from "../../contexts/User/User.context";
+import * as Styled from './MedicalRecord.style';
+
 export const MedicalRecordPage = () => {
+    const {user} = useContext(UserContext);
+    const {setToolbar} = useContext(ToolbarContext);
+
+    const setHomeToolbar = () => {
+        const newToolbar = {
+            title: 'Prontuário',
+            userName: user.name
+        };
+        setToolbar(newToolbar)
+    };
+
+    useEffect(() => setHomeToolbar(), []);
     return (
         <>
-            <p>MedicalRecord Page is render</p>
+        <ToolbarComponent/>
+            <Styled.MedicalRecord>
+                <Styled.PatientName>Paciente {'Jane Doe'}</Styled.PatientName>
+                <Styled.Container>
+                    <Styled.PatientInfos><strong>Convênio:</strong> {'Unimed'}</Styled.PatientInfos>
+                    <Styled.PatientInfos><strong>Contato de emergência:</strong> {'(99) 9999-9999'}</Styled.PatientInfos>
+                    <Styled.PatientInfos><strong>Alergias:</strong> {'Lorem Ipsun'}</Styled.PatientInfos>
+                    <Styled.PatientInfos><strong>Cuidados específicos:</strong> {'Lorem Ipsun'}</Styled.PatientInfos>
+                </Styled.Container>
+
+            </Styled.MedicalRecord>
         </>
     )
 }
---
+++ src/pages/MedicalRecord/MedicalRecord.style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const MedicalRecord = styled.div`
+    padding: 2rem 4rem;
+    width: 100%;
+`
+export const PatientName = styled.h2`
+    color: #9A95B1;
+    margin-bottom: 1rem;
+`
+
+export const Container = styled.div`
+    margin-left: 1rem;
+`
+
+export const PatientInfos = styled.h3`
+    font-weight: 400;
+    color: #25232F;
+    margin-bottom: 0.3rem;
+`
