1436735b98043c59f3ac6aa63c2eb0ac175c6d5f
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-20 21:36:27 -0300

    Iniciando estilização e importações

--- src/pages/Exams/Exams.page.jsx
+++ src/pages/Exams/Exams.page.jsx
@@ -1,7 +1,41 @@
+import { useContext, useEffect } from "react";
+import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component"
+import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
+import { UserContext } from "../../contexts/User/User.context";
+import * as Styled from './Exams.style';
+import { InputComponent } from "../../components/Form/Input/Input.component";
+import { FaSearch } from "react-icons/fa";
+
 export const ExamsPage = () => {
+    const {setToolbar} = useContext(ToolbarContext);
+    const {user} = useContext(UserContext);
+
+    const setHomeToolbar = () => {
+        const newToolbar = {
+            title: 'Cadastrar Exame',
+            userName: user.name
+        };
+        setToolbar(newToolbar)
+    };
+
+    useEffect(() => setHomeToolbar(), []);
     return (
         <>
-            <p>ExamPage is render</p>
+            <ToolbarComponent/>
+            <Styled.Exams>
+            <Styled.Title>Encontre o paciente</Styled.Title>
+            <Styled.SearchPatient>
+                <Styled.InputGroup>
+                    <InputComponent
+                        type='text'
+                        id='pacientSearch'
+                        placeholder='Digite o nome do paciente'
+                    />
+                    <Styled.InputIcon><FaSearch/></Styled.InputIcon>
+                </Styled.InputGroup>
+                <Styled.Button>Buscar</Styled.Button>
+            </Styled.SearchPatient>
+        </Styled.Exams>
         </>
     )
 }
