1738413d15800426369bd7e2b4aa2cc1850ef309
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-20 22:40:38 -0300

    Estilizando card de listagem de prontuário

--- src/components/ListPatientCard/ListPatientCard.component.jsx
+++ src/components/ListPatientCard/ListPatientCard.component.jsx
@@ -1,7 +1,20 @@
+import PropTypes from 'prop-types';
+import * as Styled from './ListPatientCard.style';
+import {GrNext} from 'react-icons/gr'
+
 export const ListPatientCardComponent = ({id, name, insurance}) => {
     return (
-        <>
-            <p>ListPatient Card is render</p>
-        </>
+        <Styled.ListCard>
+            <Styled.PatientInfo>{id}</Styled.PatientInfo>
+            <Styled.PatientInfo>{name}</Styled.PatientInfo>
+            <Styled.PatientInfo>{insurance}</Styled.PatientInfo>
+            <Styled.Icon><GrNext/></Styled.Icon>
+        </Styled.ListCard>
     )
+};
+
+ListPatientCardComponent.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    insurance: PropTypes.string
 }
