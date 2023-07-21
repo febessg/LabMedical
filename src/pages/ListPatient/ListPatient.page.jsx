822a815749fb9fc8c5e17bff3cb67e4bfb364162
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-20 22:08:42 -0300

    Iniciando estilização e importações

--- src/pages/ListPatient/ListPatient.page.jsx
+++ src/pages/ListPatient/ListPatient.page.jsx
@@ -1,7 +1,48 @@
+import { useContext, useEffect } from "react";
+import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component"
+import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
+import { UserContext } from "../../contexts/User/User.context";
+import { InputComponent } from "../../components/Form/Input/Input.component";
+import { FaSearch } from "react-icons/fa";
+import * as Styled from './ListPatient.style';
+
 export const ListPatientPage = () => {
+    const {setToolbar} = useContext(ToolbarContext);
+    const {user} = useContext(UserContext);
+
+    const setHomeToolbar = () => {
+        const newToolbar = {
+            title: 'Listagem de Prontuários',
+            userName: user.name
+        };
+        setToolbar(newToolbar)
+    };
+
+    useEffect(() => setHomeToolbar(), []);
     return (
         <>
-            <p>ListPatient Page is render</p>
+            <ToolbarComponent/>
+            <Styled.ListPage>
+            <Styled.Title>Encontre o paciente</Styled.Title>
+            <Styled.SearchPatient>
+                <Styled.InputGroup>
+                    <InputComponent
+                        type='text'
+                        id='pacientSearch'
+                        placeholder='Digite o nome do paciente'
+                    />
+                    <Styled.InputIcon><FaSearch/></Styled.InputIcon>
+                </Styled.InputGroup>
+                <Styled.Button>Buscar</Styled.Button>
+            </Styled.SearchPatient>
+            <Styled.List>
+                <Styled.SubTitles>
+                    <h3>Registro</h3>
+                    <h3>Nome do Paciente</h3>
+                    <h3>Convênio</h3>
+                </Styled.SubTitles>
+            </Styled.List>
+        </Styled.ListPage>
         </>
     )
 }
