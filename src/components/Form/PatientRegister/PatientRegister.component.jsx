cb999e3720947dda8a6b1da0eb145a154a011221
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-23 11:31:05 -0300

    Iniciando validação cadastro de paciente

--- src/components/Form/PatientRegister/PatientRegister.component.jsx
+++ src/components/Form/PatientRegister/PatientRegister.component.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { InputComponent } from "../Input/Input.component";
 import * as Styled from './PatientRegister.style';
 
@@ -14,29 +15,126 @@ export const FormPatientRegisterComponent = () => {
         {value: 'opt4', label: 'Viúvo'},
         {value: 'opt5', label: 'União estável'}
     ]
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors}
+    } = useForm();
+
     return (
         <Styled.Form>
-            
             <Styled.InputGroup className="InputGroup">
                 <Styled.Titles>Identificação</Styled.Titles>
-                <InputComponent label='Nome Completo' id='fullName' type='text'/>
+                <InputComponent 
+                    label='Nome Completo' 
+                    id='fullName' 
+                    type='text' 
+                    register={{...register('fullName', {
+                        required: true,
+                        minLength: {
+                            value: 5,
+                            message: 'O nome deve ter pelo menos 5 caracteres'
+                        },
+                        maxLength: {
+                            value: 50,
+                            message: 'O nome não pode exceder 50 caracteres'
+                        }
+                    })}}
+                    error= {errors.fullName}
+                />
                 <Styled.InputRow className="InputRow">
-                    <InputComponent label='Gênero' id='gender' type='select' options={gender}/>
-                    <InputComponent label='Data de Nascimento' id='birthDate' type='date'/>
-                    <InputComponent label='Estado Civil' id='maritalStatus' type='select' options={maritalStatus}/>
+                    <InputComponent 
+                    label='Gênero' 
+                    id='gender' 
+                    type='select' 
+                    options={gender}
+                    register={{...register('gender', {
+                        required: true, 
+                    })}}
+                    error={errors.gender}
+                    />
+                    <InputComponent label='Data de Nascimento' id='birthDate' type='date'
+                    register={{...register('birthDate', {
+                        required: true,
+                        pattern: {
+                            value: /^\d{2}-\d{2}-\d{4}$/,
+                            message: "Data inválida. Utilize o formato 'dd-mm-aaaa'"
+                        }
+                    })}}
+                        error={errors.birthDate}
+                    />
+                    <InputComponent label='Estado Civil' id='maritalStatus' type='select' options={maritalStatus}
+                    register={{...register('maritalStatus', {
+                        required: true, 
+                    })}}
+                    error={errors.maritalStatus}
+                    />
                 </Styled.InputRow>
                 <Styled.InputRow className="InputRow">
-                    <InputComponent label='CPF' id='cpf' type='text' placeholder='000.000.000-00'/>
-                    <InputComponent label='RG' id='rg' type='text'/>
-                    <InputComponent label='Naturalidade' id='nationality' type='text'/>
+                    <InputComponent label='CPF' id='cpf' type='text' placeholder='000.000.000-00'
+                    register={{...register('cpf', {
+                        required: true,
+                        pattern: {
+                            value: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+                            message: 'CPF inválido.'
+                        }
+                    })}}
+                    error={errors.cpf}    
+                    />
+                    <InputComponent label='RG' id='rg' type='text'
+                    register={{...register('rg', {
+                        required: true,
+                        maxLength: 20
+                    })}}
+                    error= {errors.rg}
+                    />
+                    <InputComponent label='Naturalidade' id='nationality' type='text'
+                    register={{...register('nationality', {
+                        required: true,
+                        minLength: {
+                            value: 5
+                        },
+                        maxLength: {
+                            value: 50
+                        }
+                    })}}
+                    error= {errors.nationality}
+                    />
                 </Styled.InputRow>
                 <Styled.InputRow className="InputRow">
-                    <InputComponent label='Telefone' id='phoneNumber' type='text' placeholder='(99) 9 9999-99999'/>
-                    <InputComponent label='E-mail' id='email' type='email'/>
-                    <InputComponent label='Contato de Emergência' id='emergencyContact' type='text' placeholder='(99) 9 9999-99999'/>
+                    <InputComponent label='Telefone' id='phoneNumber' type='text' placeholder='(99) 9 9999-9999'
+                    register={{...register('phoneNumber', {
+                        required: true,
+                        pattern: {
+                            value: /^\(\d{2}\) \d \d{4}-\d{4}$/,
+                            message: 'Telefone inválido.'
+                        }
+                    })}}
+                    error={errors.phoneNumber}
+                    />
+                    <InputComponent label='E-mail' id='email' type='email'
+                    register={{...register('email', {
+                        required: true,
+                        validate:  {matchPath: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)}
+                    })}}
+                    />
+                    <InputComponent label='Contato de Emergência' id='emergencyContact' type='text' placeholder='(99) 9 9999-9999'
+                    register={{...register('emergencyContact', {
+                        required: true,
+                        pattern: {
+                            value: /^\(\d{2}\) \d \d{4}-\d{4}$/,
+                            message: 'Telefone inválido.'
+                        }
+                    })}}
+                    error={errors.emergencyContact}
+                    />
                 </Styled.InputRow>
                 <Styled.InputRow className="InputRow">
-                    <InputComponent label='Alergias' id='allergies' type='textarea'/>
+                    <InputComponent label='Alergias' id='allergies' type='textarea'
+                    
+                    />
                     <InputComponent label='Cuidados' id='specialCares' type='textarea'/>
                 </Styled.InputRow>
             </Styled.InputGroup>
@@ -68,7 +166,7 @@ export const FormPatientRegisterComponent = () => {
             <Styled.ButtonsWrapper>
                     <Styled.Button>Editar</Styled.Button>
                     <Styled.Button $color='red'>Deletar</Styled.Button>
-                    <Styled.Button>Salvar</Styled.Button>
+                    <Styled.Button $active={!errors}>Salvar</Styled.Button>
             </Styled.ButtonsWrapper>
         </Styled.Form>
     )
