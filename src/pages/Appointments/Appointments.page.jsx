b05264a3db8d6066a83f19db2d97f1686ada38df
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-28 15:27:15 -0300

    Configurando barra de pesquisa pag de consultas

--- src/pages/Appointments/Appointments.page.jsx
+++ src/pages/Appointments/Appointments.page.jsx
@@ -2,41 +2,81 @@ import { InputComponent } from "../../components/Form/Input/Input.component";
 import {FaSearch} from 'react-icons/fa';
 import * as Styled from './Appointments.style';
 import { ToolbarComponent } from "../../components/Toolbar/Toolbar.component";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ToolbarContext } from "../../contexts/Toolbar/Toolbar.context";
 import { UserContext } from "../../contexts/User/User.context";
 import { FormAppointmentComponent } from "../../components/Form/Appointment/Appointment.component";
+import { useForm } from "react-hook-form";
+import { PatientService } from "../../services/Patient/Patient.service";
+import { ListPatientCardComponent } from "../../components/ListPatientCard/ListPatientCard.component";
 
 export const AppointmentsPage = () => {
     const {setToolbar} = useContext(ToolbarContext);
     const {user} = useContext(UserContext);
 
-    const setHomeToolbar = () => {
+    useEffect(() => {
         const newToolbar = {
-            title: 'Cadastrar Consultas',
-            userName: user.name
+          title: 'Cadastrar Consultas',
+          userName: user.name
         };
-        setToolbar(newToolbar)
-    };
+        setToolbar(newToolbar);
+      }, [user.name]);
 
-    useEffect(() => setHomeToolbar(), []);
+      const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm();
+
+    const [search, setSearch] = useState();
+
+    const submitForm = (data) => {
+        const {name} = data;
+
+        const patientByName = PatientService.ShowByName(name);
+
+        patientByName ? setSearch(patientByName) : alert('Paciente não encontrado');
+
+        reset()
+    }
+
+    const registerAppointment = (data) => {
+        <FormAppointmentComponent patient={data.fullName}/>
+    }
+
     return(
         <>
         <ToolbarComponent/>
         <Styled.Appointments>
             <Styled.Title>Encontre o paciente</Styled.Title>
-            <Styled.SearchPatient>
+            <Styled.SearchPatient onSubmit={handleSubmit(submitForm)}>
                 <Styled.InputGroup>
                     <InputComponent
                         type='text'
-                        id='pacientSearch'
+                        id='name'
                         placeholder='Digite o nome do paciente'
+                        register={{...register('name')}}
                     />
                     <Styled.InputIcon><FaSearch/></Styled.InputIcon>
                 </Styled.InputGroup>
-                <Styled.Button>Buscar</Styled.Button>
+                <Styled.Button type="submit">Buscar</Styled.Button>
             </Styled.SearchPatient>
-            <FormAppointmentComponent patient='Jane Doe'/>
+            {!search ? PatientService.Get().map((patient) => {
+                return <ListPatientCardComponent
+                    key={patient.id}
+                    id={patient.id}
+                    name={patient.fullName}
+                    insurance={patient.insurance}
+                    func={registerAppointment}        
+                />
+            }) : <ListPatientCardComponent
+            id={search.id}
+            name={search.fullName}
+            insurance={search.insurance}
+            func={registerAppointment}   
+            />
+            }
+            
         </Styled.Appointments>
         </>
         
